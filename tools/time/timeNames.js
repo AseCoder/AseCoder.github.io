8cdc53b1facfe7ac96464a101fb6f689963ff70c
AseCoder <51131379+AseCoder@users.noreply.github.com>
2021-08-04 23:06:12 +0300

    time tool

---
+++ tools/time/timeNames.js
@@ -0,0 +1,30 @@
+const timeNames = [
+	{
+		name: 'year',
+		length: 1000 * 60 * 60 * 24 * 30.436875 * 12
+	},
+	{
+		name: 'month',
+		length: 1000 * 60 * 60 * 24 * 30
+	},
+	{
+		name: 'week',
+		length: 1000 * 60 * 60 * 24 * 7
+	},
+	{
+		name: 'day',
+		length: 1000 * 60 * 60 * 24
+	},
+	{
+		name: 'hour',
+		length: 1000 * 60 * 60
+	},
+	{
+		name: 'minute',
+		length: 1000 * 60
+	},
+	{
+		name: 'second',
+		length: 1000
+	}
+]
